afd3111a2261e558d3019874ec0042973073d4b9
kali kali <illia.tymofeev2015@gmail.com>
2026-06-14 18:39:28 +0300

    houmlender

--- Mytt/settings.py
+++ Mytt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zo5wb_yylpw&*2u10zlzpsngti3(mle)qwuf&wkrfgvri&ofj@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
